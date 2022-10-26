ce0a205ac8112202476e16971a3ced09f563dd4b
Nemo3003 <ulimarian2000@gmail.com>
2022-10-26 11:12:43 -0300

    [UPD] Auth0 almost fixed

--- routes/index.js
+++ routes/index.js
@@ -4,11 +4,12 @@ const router = express.Router();
 const openCors = require("../middleware/openCors");
 const path = require('path');
 router.use(express.static('static'));
+const {  requiresAuth } = require('express-openid-connect');
 
 
 router.use('/', require('./swagger'));
-router.use('/app/games', require('./games'));
-router.use('/app/companies', require('./companies'));
+router.use('/app/games', requiresAuth() ,require('./games'));
+router.use('/app/companies', requiresAuth(), require('./companies'));
 
 module.exports = router;
 
