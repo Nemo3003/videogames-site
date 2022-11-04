c9b202b485e2fe97fa0d791344dbf33ccdb0670b
Nemo3003 <ulimarian2000@gmail.com>
2022-11-04 14:13:25 -0300

    [UPD] companies not here

--- src/routes/index.js
+++ src/routes/index.js
@@ -8,7 +8,7 @@ router.use(express.static('static'));
 router.use('/', require('./swagger'));
 router.use('/', require('./users'))
 router.use('/app/games',require('./games'));
-router.use('/app/companies', require('./companies'));
+
 
 module.exports = router;
 
